c357ff26215ef0dfe126ef1b4972c1fc69d03c7f
wikoj <wikoj1021@gmail.com>
2023-01-10 22:08:25 +0100

    duck mountains

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,54 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const worldPath = document.getElementById("world");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const points: number[] = [250];
+const maxPoints = 50;
+const pointMax = 20;
+let currentPoint = 0;
+let mountainLen = 3;
+
+const interpolateCos = (a: number, b: number, x: number) => {
+  const ft = x * Math.PI;
+  const f = (1 - Math.cos(ft)) * 0.5;
+  return a * (1 - f) + b * f;
+};
+
+const makePathFromPoints = () => {
+  return points.reduce((path, point, i) => {
+    if (i === 0) path = `M 0, ${point}`;
+    path += `L ${i * (500 / maxPoints)}, ${point}`;
+    return path;
+  }, "");
+};
+
+let p1 = Math.random();
+let p2 = Math.random();
+const getNextPerlinNoise = () => {
+  let point;
+
+  if (currentPoint % mountainLen === 0) {
+    p1 = p2;
+    p2 = Math.random();
+    point = 500 / 2 + p1 * pointMax;
+  } else {
+    point =
+      500 / 2 +
+      interpolateCos(p1, p2, (currentPoint % mountainLen) / mountainLen) *
+        pointMax;
+  }
+
+  points.unshift(point);
+  currentPoint++;
+};
+
+const step = () => {
+  getNextPerlinNoise();
+  worldPath?.setAttribute("d", makePathFromPoints());
+};
+
+for (let i = 0; i < maxPoints; i++) {
+  getNextPerlinNoise();
+}
+
+window.setInterval(step, 100);
